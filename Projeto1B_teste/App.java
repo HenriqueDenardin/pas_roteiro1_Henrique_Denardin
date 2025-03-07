8e29f54cb0d2c8ec0b49f99cc237806dd9986a19
HenriqueDenardin <136604147+HenriqueDenardin@users.noreply.github.com>
2025-03-07 01:13:05 +0000

    Final

---
+++ Projeto1B_teste/App.java
@@ -0,0 +1,16 @@
+package Projeto1B_teste;
+import java.util.Scanner;
+public class App {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("nome do professor?");
+        String nome = in.next();
+        System.out.println("Quantos anos de experiencia ele tem?");
+        int experiencia = in.nextInt();
+        Professor p = new Professor(nome, experiencia);
+        System.out.println(p.toString());
+        System.out.println("Categoria: " + p.classifica());
+        in.close();
+    }
+    
+}
